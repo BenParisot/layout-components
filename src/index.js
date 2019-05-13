95a1bb5e607059ffc0a711bf47305eb022b5af1b
Ben Parisot <me@benparisot.com>
2019-05-13 11:53:27 -0700

    get html template working with js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-console.log('oh hello');
-console.log('hi there!');
+//append a ul
+//append an li with 'spot', 'rover', 'bingo', 'joe'
+
+const rootNode = document.getElementById('root');
+const headerNode = document.getElementById('header');
+
+const heading = document.createElement('h1');
+heading.textContent = 'These are my dogs yo';
+headerNode.appendChild(heading);
+
+const list = document.createElement('ul');
+const dogNames = [
+  'Spot',
+  'Rover',
+  'Bingo',
+  'Joe'
+];
+rootNode.appendChild(list);
+
+dogNames.forEach(name => {
+  const nameLi = document.createElement('li');
+  nameLi.textContent = name;
+  list.appendChild(nameLi);
+});
+
